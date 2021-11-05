55e2f333d450f424de3ff396a6200b38d4bd7986
josef <53139929+gdjosef@users.noreply.github.com>
2021-11-05 18:35:01 +0800

    New article time!
    + small design changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+window.onload = setSubtitle;
+
 const clipboard = new ClipboardJS("#discord");
 
 clipboard.on("success", function(e) {
@@ -6,3 +8,17 @@ clipboard.on("success", function(e) {
   setTimeout(() => e.trigger.innerHTML = name, 1000);
   e.clearSelection();
 });
+
+const subtitles = [
+  "I'll move this to a proper framework eventually.",
+  "No mom, I'm too busy making a new blog post!",
+  "Being mobile-friendly, one tag at a time.",
+  "You will never see this appear on the webpage",
+  "I'll add more later, boutta eat dinner."
+]
+
+const subtitle = document.getElementById("subtitle");
+
+function setSubtitle() {
+  subtitle.textContent = subtitles[Math.floor(Math.random() * subtitles.length)];
+}
