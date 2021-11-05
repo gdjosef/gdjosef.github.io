5c5cffec2330d1fb8de797667943c84829a7a549
josef <53139929+gdjosef@users.noreply.github.com>
2021-11-05 17:44:24 +0800

    Added social media links

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const clipboard = new ClipboardJS('#discord');
+const clipboard = new ClipboardJS("#discord");
 
-clipboard.on('success', function(e) {
+clipboard.on("success", function(e) {
   let name = e.trigger.innerHTML;
-  e.trigger.innerHTML = 'tag copied';
+  e.trigger.innerHTML = "copied";
   setTimeout(() => e.trigger.innerHTML = name, 1000);
   e.clearSelection();
 });
